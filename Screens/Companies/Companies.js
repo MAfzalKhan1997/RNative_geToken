ea5017e7de3b8c92c869679f61278275d56bb502
Muhammad Afzal Khan <mafzalkhan1997@gmail.com>
2018-12-25 01:12:13 +0500

    token app

--- Screens/Companies/Companies.js
+++ Screens/Companies/Companies.js
@@ -1,7 +1,7 @@
 import React from 'react';
 // import { AsyncStorage, View } from 'react-native';
 import { Container, Content, Button, Text } from 'native-base';
-// import { Drawer } from 'native-base';
+import { Modal, TouchableHighlight, View, Alert } from 'react-native';
 
 // import AuthState from '../../Helper/AuthState'
 // import firebase from '../../Config/firebase';
@@ -11,6 +11,18 @@ export default class Companies extends React.Component {
     static navigationOptions = {
         title: 'All Companies',
     };
+
+    constructor() {
+        super()
+
+        this.state = {
+            modalVisible: false,
+        };
+    }
+
+    setModalVisible(visible) {
+        this.setState({ modalVisible: visible });
+    }
 
     // componentWillMount() {
     //     AuthState()
@@ -23,16 +35,64 @@ export default class Companies extends React.Component {
     // }
 
     addCompany() {
-
+        alert()
     }
 
     render() {
         return (
             <Container>
                 <Content padder>
-                    <Button block onPress={() => this.addCompany()}>
+
+                    <Modal
+                        animationType="slide"
+                        transparent={false}
+                        visible={this.state.modalVisible}
+                        onRequestClose={() => {
+                            alert('Modal has been closed.');
+                        }}>
+                        <View>
+
+                            <View
+                            // style={{
+                            //     flex: 1,
+                            //     flexDirection: 'row',
+                            //     justifyContent: 'center',
+                            //     alignItems: 'stretch',
+                            // }}
+                            >
+
+                                <Button full
+                                    // style={{
+                                    //     flex: 1,
+                                    //     // flexDirection: 'row',
+                                    //     justifyContent: 'center',
+                                    //     alignItems: 'center',
+                                    // }}
+                                    onPress={() => this.setModalVisible(false)}>
+                                    <Text>cancel</Text>
+                                </Button>
+                                <Button full
+                                    // style={{
+                                    //     flex: 1,
+                                    //     // flexDirection: 'row',
+                                    //     justifyContent: 'center',
+                                    //     alignItems: 'center',
+                                    // }}
+                                    info onPress={() => this.addCompany()}>
+                                    <Text>add my company</Text>
+                                </Button>
+
+                            </View>
+                        </View>
+                    </Modal>
+
+
+                    <Button block onPress={() => {
+                        this.setModalVisible(true)
+                    }}>
                         <Text>add</Text>
                     </Button>
+
                 </Content>
             </Container>
         );
