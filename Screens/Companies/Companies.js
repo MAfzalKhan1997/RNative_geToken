53da6e50f555ea8c193b44c783903610ff8376bc
Muhammad Afzal Khan <mafzalkhan1997@gmail.com>
2018-12-27 03:49:29 +0500

    token app

--- Screens/Companies/Companies.js
+++ Screens/Companies/Companies.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { Modal, TouchableHighlight, View, Alert, Image, } from 'react-native';
-import { Container, Content, Button, Text, Form, Item, Input, Label } from 'native-base';
+import { Modal, TouchableHighlight, TouchableOpacity, View, Alert, Image, } from 'react-native';
+import { Container, Content, Button, Text, Form, Item, Input, Label, List, ListItem, Icon, Left, Body, Right, } from 'native-base';
 import { Permissions, ImagePicker } from 'expo';
 
 // import AuthState from '../../Helper/AuthState'
@@ -17,45 +17,20 @@ export default class Companies extends React.Component {
 
         this.state = {
             modalVisible: false,
+            innerModalVisible: false,
 
             images: null,
         };
     }
 
-    // async componentDidMount() {
-    //     let status;
-    //     status = await Permissions.getAsync(Permissions.CAMERA);
-    //     let statusCam = status.status;
-    //     console.log("Camera Permissions: ", statusCam);
-
-    //     status = await Permissions.getAsync(Permissions.CAMERA_ROLL);
-    //     let statusCamRoll = status.status;
-    //     console.log("Camera_Roll Permissions: ", statusCamRoll);
-
-    //     if (statusCam !== "granted") {
-    //         console.log("Requesting Camera Permissions");
-    //         status = await Permissions.askAsync(Permissions.CAMERA);
-    //         statusCam = status.status;
-    //     }
-
-    //     if (statusCamRoll !== "granted") {
-    //         console.log("Requesting Camera_Roll Permissions");
-    //         status = await Permissions.askAsync(Permissions.CAMERA_ROLL);
-    //         statusCamRoll = status.status;
-    //     }
-
-    //     if (statusNotifications !== "granted" || statusLocation !== "granted") {
-    //         console.log("Permissions not granted");
-    //         return;
-    //     }
-
-    //     console.log("Permissions Granted!");
-    // }
 
     setModalVisible(visible) {
         this.setState({ modalVisible: visible });
     }
 
+    setInnerModalVisible(visible) {
+        this.setState({ innerModalVisible: visible });
+    }
     // componentWillMount() {
     //     AuthState()
     //     // this.check()
@@ -160,27 +135,39 @@ export default class Companies extends React.Component {
                         }}>
                         <View>
 
-                            <Form>
-                                <Item floatingLabel>
-                                    <Label>Company Name</Label>
-                                    <Input />
-                                </Item>
-                                <Item floatingLabel last >
-                                    <Label>Since</Label>
-                                    <Input keyboardType='numeric' />
-                                </Item>
+                            <View>
+                                <Form>
+                                    <Item floatingLabel>
+                                        <Label>Company Name</Label>
+                                        <Input />
+                                    </Item>
+                                    <Item floatingLabel last >
+                                        <Label>Since</Label>
+                                        <Input keyboardType='numeric' />
+                                    </Item>
 
-                                <Button full success
-                                    onPress={this._pickImage}>
-                                    <Text>Pick an image from camera roll</Text>
-                                </Button>
+                                    <Button full
+                                        onPress={() =>
+                                            Alert.alert(
+                                                'Upload Image',
+                                                'Choose...',
+                                                [
+                                                    { text: 'Cancel', onPress: () => console.log('Cancel Pressed'), style: 'cancel' },
+                                                    { text: 'Camera', onPress: this._clickImage },
+                                                    { text: 'Gallery', onPress: this._pickImage },
+                                                ],
+                                            )
+                                        }>
+                                        <Text>upload image</Text>
+                                    </Button>
 
-                                {image &&
-                                    <Image source={{ uri: image }} style={{ width: 200, height: 200 }} />}
-                            </Form>
+
+                                    {image &&
+                                        <Image source={{ uri: image }} style={{ width: 200, height: 200 }} />}
+                                </Form>
+                            </View>
 
                             <View>
-
                                 <Button full
                                     onPress={() => this.setModalVisible(false)}>
                                     <Text>cancel</Text>
@@ -190,11 +177,10 @@ export default class Companies extends React.Component {
                                     info onPress={() => this.addCompany()}>
                                     <Text>add my company</Text>
                                 </Button>
-
                             </View>
+
                         </View>
                     </Modal>
-
 
                     <Button block onPress={() => {
                         this.setModalVisible(true)
